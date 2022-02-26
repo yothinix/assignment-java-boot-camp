2a8f5681aaae7f4b57599ce3ffe3689411b7d4d0
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 16:12:45 +0700

    :card_file_box: #1 Add Product Entity

---
+++ ecommerce/src/main/java/com/yothinix/ecommerce/products/Product.java
@@ -0,0 +1,24 @@
+package com.yothinix.ecommerce.products;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private Integer id;
+    private String name;
+    private String brand;
+    private String category;
+    private String description;
+    private String price;
+}
