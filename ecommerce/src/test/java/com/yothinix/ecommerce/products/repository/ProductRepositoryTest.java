6a949363618bb97f1e5ae74483e27ccf2b37c133
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 18:01:38 +0700

    :recycle: #2 Remove unused import

--- ecommerce/src/test/java/com/yothinix/ecommerce/products/repository/ProductRepositoryTest.java
+++ ecommerce/src/test/java/com/yothinix/ecommerce/products/repository/ProductRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.yothinix.ecommerce.products.repository;
 
 import com.yothinix.ecommerce.products.entity.Product;
-import com.yothinix.ecommerce.products.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
