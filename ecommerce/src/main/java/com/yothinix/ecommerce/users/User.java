be9a54d71108b65d1d580b53f01a8f47f338eb99
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 21:26:30 +0700

    :sparkles: #3 Add User repository

---
+++ ecommerce/src/main/java/com/yothinix/ecommerce/users/User.java
@@ -0,0 +1,20 @@
+package com.yothinix.ecommerce.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    private Integer id;
+    private String username;
+}
---
+++ ecommerce/src/test/java/com/yothinix/ecommerce/users/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.yothinix.ecommerce.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void findByIdShouldReturnUserTest() {
+        User user = new User(1, "yothinix");
+        testEntityManager.persist(user);
+
+        Optional<User> userOptional = userRepository.findById(1);
+        User actual = userOptional.get();
+
+        assertEquals(1, actual.getId());
+        assertEquals("yothinix", actual.getUsername());
+    }
+
+    @Test
+    void findByIdShouldReturnEmptyWhenUserNotFoundTest() {
+        Optional<User> actual = userRepository.findById(1);
+
+        assertTrue(actual.isEmpty());
+    }
+}
