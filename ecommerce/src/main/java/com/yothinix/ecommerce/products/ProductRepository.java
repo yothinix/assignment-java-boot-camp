e96e623d9bb1acae90df0b055d2842db922cc506
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 16:24:55 +0700

    :card_file_box: #1 Add find product by name contain interface

--- ecommerce/src/main/java/com/yothinix/ecommerce/products/ProductRepository.java
+++ ecommerce/src/main/java/com/yothinix/ecommerce/products/ProductRepository.java
@@ -3,6 +3,10 @@ package com.yothinix.ecommerce.products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findProductByNameContains(String name);
 }
---
+++ ecommerce/src/test/java/com/yothinix/ecommerce/products/ProductRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.yothinix.ecommerce.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    void findProductByNameContainsTextFoundProductTest() {
+        Product product1 = new Product();
+        product1.setId(1);
+        product1.setName("nike air jordan");
+        entityManager.persist(product1);
+
+        Product product2 = new Product();
+        product2.setId(2);
+        product2.setName("adidas ultra boost");
+        entityManager.persist(product2);
+
+        List<Product> actual = repository.findProductByNameContains("nike");
+
+        assertEquals(1, actual.size());
+        assertEquals("nike air jordan", actual.get(0).getName());
+    }
+
+    @Test
+    void findProductByNameContainsTextNotFoundProductTest() {
+        Product product1 = new Product();
+        product1.setId(1);
+        product1.setName("nike air jordan");
+        entityManager.persist(product1);
+
+        Product product2 = new Product();
+        product2.setId(2);
+        product2.setName("adidas ultra boost");
+        entityManager.persist(product2);
+
+        List<Product> actual = repository.findProductByNameContains("reebok");
+
+        assertEquals(0, actual.size());
+    }
+}
