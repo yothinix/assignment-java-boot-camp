8640574cfeb83b16406c7fd93d16972fc5cbd489
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 21:52:39 +0700

    :bug: #3 Add @Repository to payments and all users repository

--- ecommerce/src/main/java/com/yothinix/ecommerce/users/repository/UserAddressRepository.java
+++ ecommerce/src/main/java/com/yothinix/ecommerce/users/repository/UserAddressRepository.java
@@ -2,6 +2,8 @@ package com.yothinix.ecommerce.users.repository;
 
 import com.yothinix.ecommerce.users.entity.UserAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAddressRepository extends JpaRepository<UserAddress, Integer> {
 }
--- ecommerce/src/main/java/com/yothinix/ecommerce/users/repository/UserRepository.java
+++ ecommerce/src/main/java/com/yothinix/ecommerce/users/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.yothinix.ecommerce.users.repository;
 
 import com.yothinix.ecommerce.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
