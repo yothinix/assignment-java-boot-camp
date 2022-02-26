c66f3f0dc1f150ed8e32c25351c29efcdd8dfcbc
Yothin Muangsommuk <m@yothinix.com>
2022-02-26 16:05:36 +0700

    :tada: #1 Initial Spring project

---
+++ ecommerce/src/test/java/com/yothinix/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yothinix.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
